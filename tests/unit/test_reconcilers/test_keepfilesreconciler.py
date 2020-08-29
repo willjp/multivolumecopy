92f5c489648be2833f7c89bdfe5f3ae0bfac08ae
Will Pittman <willjpittman@gmail.com>
2020-08-28 23:08:16 -0400

    continued writing tests

--- tests/unit/test_reconcilers/test_keepfilesreconciler.py
+++ tests/unit/test_reconcilers/test_keepfilesreconciler.py
@@ -1,12 +1,65 @@
 from multivolumecopy.reconcilers import keepfilesreconciler
+from multivolumecopy.resolvers import resolver
+from multivolumecopy import copyoptions
+import mock
+import os
+import contextlib
+
+
+NS = keepfilesreconciler.__file__
+
+
+@contextlib.contextmanager
+def mock_walk(walk_paths=None):
+    walk_paths = walk_paths or {}
+    def walk_results(srcpath, *args, **kwargs):
+        if srcpath not in walk_paths:
+            return os.walk(srcpath, *args, **kwargs)
+        for copyfile in walk_paths[srcpath]:
+            yield copyfile
+
+    with mock.patch('os.walk', side_effect=walk_results) as mock_walk:
+        yield mock_walk
 
 
 class TestKeepFilesReconciler:
-    def test_removes_unrelated_files(self):
-        assert False
+    def setup(self):
+        self.copyfiles = tuple([
+            resolver.CopyFile(src='/src/0.txt', dst='/dst/0.txt',
+                              relpath='0.txt', bytes=1025, index=0),
+            resolver.CopyFile(src='/src/a/1.txt', dst='/dst/a/1.txt',
+                              relpath='a/1.txt', bytes=1025, index=1),
+            resolver.CopyFile(src='/src/a/2.txt', dst='/dst/a/2.txt',
+                              relpath='a/2.txt', bytes=1025, index=2),
+        ])
+        self.resolver = mock.Mock()
 
-    def test_removes_already_copied_files(self):
-        assert False
+        self.options = copyoptions.CopyOptions()
+        self.options.dst = '/dst'
+
+    @mock.patch('os.remove')
+    def test_removes_unrelated_files(self, m_remove):
+        walk_paths = {
+            '/dst': [('/dst', ['/a'], ['x0.txt']),
+                     ('/dst/a', [], ['x1.txt'])],
+        }
+        copied_indexes = []
+        reconciler = keepfilesreconciler.KeepFilesReconciler(self.resolver, self.options)
+        with mock_walk(walk_paths):
+            reconciler.reconcile(self.copyfiles, copied_indexes)
+        m_remove.assert_has_calls([mock.call('/dst/a/x1.txt'), mock.call('/dst/a/x0.txt')])
+
+    @mock.patch('os.remove')
+    def test_removes_already_copied_files(self, m_remove):
+        """ device_index indicates the first index to be copied to the currently
+        mounted backup device. Files related to earlier indexes should be removed
+        (since they were already backed up on another device).
+        """
+        copied_indexes = [0, 2]
+        reconciler = keepfilesreconciler.KeepFilesReconciler(self.resolver, self.options)
+        with mock_walk({}):
+            reconciler.reconcile(self.copyfiles, copied_indexes)
+        m_remove.assert_has_calls([mock.call('/dst/0.txt'), mock.call('/dst/a/2.txt')])
 
     def test_removes_files_device_will_not_have_room_for(self):
         assert False
---
+++ tests/unit/test_resolvers/test_jobfileresolver.py
@@ -0,0 +1,28 @@
+from multivolumecopy.resolvers import jobfileresolver
+from multivolumecopy import copyoptions
+import mock
+
+
+FS = jobfileresolver.__name__
+
+
+class TestJobFileResolver:
+    def setup(self):
+        self.options = copyoptions.CopyOptions()
+        self.options.dst = '/dst'
+
+    def test_loads_json(self):
+        resolver = jobfileresolver.JobFileResolver("/var/tmp/.mvcopy-jobfile.json", self.options)
+        read_mock = mock.mock_open(read_data='[["/src/a/b.txt", "/dst/a/b.txt", "a/b.txt", 1024, 0]]')
+        with mock.patch('{}.open'.format(FS), read_mock):
+            copyfiles = resolver.get_copyfiles()
+            # important for mem usage
+            assert isinstance(copyfiles, tuple)
+            copyfile = copyfiles[0]
+            assert copyfile.src == "/src/a/b.txt"
+            assert copyfile.dst == "/dst/a/b.txt"
+            assert copyfile.relpath == "a/b.txt"
+            assert copyfile.bytes == 1024
+            assert copyfile.index == 0
+
+
